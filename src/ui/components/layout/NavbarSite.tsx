af1d6c13ed5aabb73e9e0d3aad0607d1e68fe6e2
GBRbotelho <gabrielbbotelho05@gmail.com>
2024-06-19 18:32:28 -0300

    Navbar Event Scroll

--- src/ui/components/layout/NavbarSite.tsx
+++ src/ui/components/layout/NavbarSite.tsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function NavbarSite() {
+  const [navbarBackground, setNavbarBackground] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = () => {
+    const position = window.scrollY;
+    if (position > 550) {
+      setNavbarBackground(true);
+    } else {
+      setNavbarBackground(false);
+    }
+
+    const scrollThreshold = 60;
+
+    if (position < scrollThreshold) {
+      setVisible(true);
+    } else {
+      const visible = prevScrollPos > position;
+      setVisible(visible);
+    }
+
+    setPrevScrollPos(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible]);
+
   return (
-    <nav className="fixed z-50 w-full flex justify-center items-center navbarBackground text-white">
+    <nav
+      className={`fixed z-50 w-full flex justify-center items-center transition duration-300 text-white ${
+        navbarBackground ? "bg-black" : "navbarBackground"
+      } ${visible ? "" : "transform -translate-y-full"}`}
+    >
       <div className="flex w-[95%] py-6 justify-between items-center">
         <div className="w-[210px]">Logo</div>
         <ul className="flex gap-6 text-[18px]">
